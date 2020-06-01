2ce3ed0b00ba1963b2a6af0755248a2eee624398
Fabio Ferrero <f.ferrero@reply.it>
2020-06-01 16:14:59 +0200

    Update project for startup and lint validation

---
+++ LoginServer/LoginServer.swift
@@ -0,0 +1,9 @@
+//
+//  LoginServer.swift
+//  LoginServer
+//
+//  Created by Fabio Ferrero on 01/06/2020.
+//  Copyright © 2020 iriscube-reply. All rights reserved.
+//
+
+import Foundation
---
+++ LoginServerTests/LoginServerTests.swift
@@ -0,0 +1,14 @@
+//
+//  LoginServerTests.swift
+//  LoginServerTests
+//
+//  Created by Fabio Ferrero on 01/06/2020.
+//  Copyright © 2020 iriscube-reply. All rights reserved.
+//
+
+import XCTest
+import LoginServer
+
+class LoginServerTests: XCTestCase {
+
+}
