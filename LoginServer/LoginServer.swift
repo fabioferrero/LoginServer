0a45699ee64e2e0645209fc3562b4e3087b9f7bd
Fabio Ferrero <f.ferrero@reply.it>
2020-06-01 22:18:41 +0200

    Implement and test login function

--- LoginServer/LoginServer.swift
+++ LoginServer/LoginServer.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+
+public enum LoginStatus: Equatable {
+    case logged
+}
+
+public enum LoginError: Error {
+    case userNotRegistered
+    case wrongUserPassword
+}
+
+public class LoginServer {
+    
+    public typealias Username = String
+    public typealias Password = String
+    
+    private var database: [Username: Password]
+    
+    public typealias Networking = (@escaping () -> Void) -> Void
+    private var networking: Networking
+    
+    public init(database: [Username: Password],
+                networking: @escaping Networking = randomDelay()) {
+        self.database = database
+        self.networking = networking
+    }
+    
+    public typealias Handler = (Result<LoginStatus, LoginError>) -> Void
+    
+    public func login(username: String, password: String, completion: @escaping Handler) {
+        networking { [weak self] in
+            guard let dbPassword = self?.database[username] else {
+                completion(.failure(.userNotRegistered)); return
+            }
+            
+            guard password == dbPassword else {
+                completion(.failure(.wrongUserPassword)); return
+            }
+            
+            completion(.success(.logged))
+        }
+    }
+}
+
+// MARK: Utility
+
+extension LoginServer {
+    
+    /// Creates a networking function that execute a given closure after a random
+    /// number of seconds taken from the given range
+    /// - Parameter range: The range that will be used to take the random delay
+    public static func randomDelay(_ range: Range<Int> = 1..<4) -> Networking {
+        return { function in
+            guard let delay: Int = range.randomElement() else { function(); return }
+            delayClosure(by: delay, closure: function)
+        }
+    }
+    
+    /// Delay a given closure by a given number of seconds, on given queue
+    static func delayClosure(by seconds: Int,
+                             on queue: DispatchQueue = .main,
+                             closure: @escaping () -> Void) {
+        let delayInSeconds: DispatchTime = .now() + .seconds(seconds)
+        queue.asyncAfter(deadline: delayInSeconds, execute: closure)
+    }
+}
--- LoginServerTests/LoginServerTests.swift
+++ LoginServerTests/LoginServerTests.swift
@@ -11,4 +11,47 @@ import LoginServer
 
 class LoginServerTests: XCTestCase {
 
+    func test_login_withEmptyDatabase_returnsUserNotRegisteredError() {
+        let sut = createSUT(database: [:])
+        
+        var result: Result<LoginStatus, LoginError>?
+        sut.login(username: "dummy", password: "dummy") { result = $0 }
+        
+        XCTAssertEqual(result, .failure(.userNotRegistered))
+    }
+    
+    func test_login_withNotRegisteredUser_returnsUserNotRegisteredError() {
+        let sut = createSUT(database: ["U1":"P1"])
+        
+        var result: Result<LoginStatus, LoginError>?
+        sut.login(username: "U2", password: "dummy") { result = $0 }
+        
+        XCTAssertEqual(result, .failure(.userNotRegistered))
+    }
+    
+    func test_login_withCorrectCredentials_succesfullyLogin() {
+        let sut = createSUT(database: ["U1":"P1"])
+        
+        var result: Result<LoginStatus, LoginError>?
+        sut.login(username: "U1", password: "P1") { result = $0 }
+        
+        XCTAssertEqual(result, .success(.logged))
+    }
+    
+    func test_login_withWrongCredentials_returnsWrongUserPasswordError() {
+        let sut = createSUT(database: ["U1":"P1"])
+        
+        var result: Result<LoginStatus, LoginError>?
+        sut.login(username: "U1", password: "dummy") { result = $0 }
+        
+        XCTAssertEqual(result, .failure(.wrongUserPassword))
+    }
+    
+    // MARK: Utility
+    private func createSUT(database: [String: String]) -> LoginServer {
+        // Mock the LoginServer with a zero-delay networking function
+        let zeroDelayFunction: LoginServer.Networking = {$0()}
+        return LoginServer(database: database, networking: zeroDelayFunction)
+    }
 }
+
